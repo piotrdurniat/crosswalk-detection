7b6a5e3bb7d92bc936468da033448dd19bd049ee
Piotr Durniat <piotr@durniat.com>
2022-06-10 23:13:17 +0200

    fix formatting

--- track.py
+++ track.py
@@ -1,24 +1,4 @@
 # limit the number of cpus used by high performance libraries
-from deep_sort.deep_sort import DeepSort
-from deep_sort.utils.parser import get_config
-from yolov5.utils.plots import Annotator, colors, save_one_box
-from yolov5.utils.torch_utils import select_device, time_sync
-from yolov5.utils.general import (LOGGER, check_img_size, non_max_suppression, scale_coords,
-                                  check_imshow, xyxy2xywh, increment_path, strip_optimizer, colorstr)
-from yolov5.utils.datasets import LoadImages, LoadStreams, VID_FORMATS
-from yolov5.models.common import DetectMultiBackend
-from yolov5.utils.downloads import attempt_download
-from yolov5.models.experimental import attempt_load
-import torch.backends.cudnn as cudnn
-import torch
-import cv2
-import numpy as np
-from pathlib import Path
-import time
-import shutil
-import platform
-import argparse
-import sys
 import os
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
@@ -26,8 +6,30 @@ os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
+import sys
 sys.path.insert(0, './yolov5')
 
+import argparse
+import os
+import platform
+import shutil
+import time
+from pathlib import Path
+import numpy as np
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+
+from yolov5.models.experimental import attempt_load
+from yolov5.utils.downloads import attempt_download
+from yolov5.models.common import DetectMultiBackend
+from yolov5.utils.datasets import LoadImages, LoadStreams, VID_FORMATS
+from yolov5.utils.general import (LOGGER, check_img_size, non_max_suppression, scale_coords,
+                                  check_imshow, xyxy2xywh, increment_path, strip_optimizer, colorstr)
+from yolov5.utils.torch_utils import select_device, time_sync
+from yolov5.utils.plots import Annotator, colors, save_one_box
+from deep_sort.utils.parser import get_config
+from deep_sort.deep_sort import DeepSort
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # yolov5 deepsort root directory
@@ -64,10 +66,8 @@ def detect(opt):
     else:  # multiple models after --yolo_model
         exp_name = "ensemble"
     exp_name = exp_name + "_" + deep_sort_model.split('/')[-1].split('.')[0]
-    # increment run if project name exists
-    save_dir = increment_path(Path(project) / exp_name, exist_ok=exist_ok)
-    (save_dir / 'tracks' if save_txt else save_dir).mkdir(parents=True,
-                                                          exist_ok=True)  # make dir
+    save_dir = increment_path(Path(project) / exp_name, exist_ok=exist_ok)  # increment run if project name exists
+    (save_dir / 'tracks' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
     # Load model
     model = DetectMultiBackend(yolo_model, device=device, dnn=opt.dnn)
@@ -75,8 +75,7 @@ def detect(opt):
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
     # Half
-    # half precision only supported by PyTorch on CUDA
-    half &= pt and device.type != 'cpu'
+    half &= pt and device.type != 'cpu'  # half precision only supported by PyTorch on CUDA
     if pt:
         model.model.half() if half else model.model.float()
 
@@ -95,8 +94,7 @@ def detect(opt):
     else:
         dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
         nr_sources = 1
-    vid_path, vid_writer, txt_path = [
-        None] * nr_sources, [None] * nr_sources, [None] * nr_sources
+    vid_path, vid_writer, txt_path = [None] * nr_sources, [None] * nr_sources, [None] * nr_sources
 
     # initialize deepsort
     cfg = get_config()
@@ -133,15 +131,13 @@ def detect(opt):
         dt[0] += t2 - t1
 
         # Inference
-        visualize = increment_path(
-            save_dir / Path(path[0]).stem, mkdir=True) if opt.visualize else False
+        visualize = increment_path(save_dir / Path(path[0]).stem, mkdir=True) if opt.visualize else False
         pred = model(im, augment=opt.augment, visualize=visualize)
         t3 = time_sync()
         dt[1] += t3 - t2
 
         # Apply NMS
-        pred = non_max_suppression(
-            pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
         dt[2] += time_sync() - t3
 
         # Process detections
@@ -163,8 +159,7 @@ def detect(opt):
                 # folder with imgs
                 else:
                     txt_file_name = p.parent.name  # get folder name containing current img
-                    # im.jpg, vid.mp4, ...
-                    save_path = str(save_dir / p.parent.name)
+                    save_path = str(save_dir / p.parent.name)  # im.jpg, vid.mp4, ...
 
             txt_path = str(save_dir / 'tracks' / txt_file_name)  # im.txt
             s += '%gx%g ' % im.shape[2:]  # print string
@@ -174,14 +169,12 @@ def detect(opt):
 
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(
-                    im.shape[2:], det[:, :4], im0.shape).round()
+                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
                 # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
-                    # add to string
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 xywhs = xyxy2xywh(det[:, 0:4])
                 confs = det[:, 4]
@@ -189,8 +182,7 @@ def detect(opt):
 
                 # pass detections to deepsort
                 t4 = time_sync()
-                outputs[i] = deepsort_list[i].update(
-                    xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
+                outputs[i] = deepsort_list[i].update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
                 t5 = time_sync()
                 dt[3] += t5 - t4
 
@@ -217,17 +209,12 @@ def detect(opt):
                         if save_vid or save_crop or show_vid:  # Add bbox to image
                             c = int(cls)  # integer class
                             label = f'{id:0.0f} {names[c]} {conf:.2f}'
-                            annotator.box_label(
-                                bboxes, label, color=colors(c, True))
-
+                            annotator.box_label(bboxes, label, color=colors(c, True))
                             if save_crop:
-                                txt_file_name = txt_file_name if (
-                                    isinstance(path, list) and len(path) > 1) else ''
-                                save_one_box(bboxes, imc, file=save_dir / 'crops' /
-                                             txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)
+                                txt_file_name = txt_file_name if (isinstance(path, list) and len(path) > 1) else ''
+                                save_one_box(bboxes, imc, file=save_dir / 'crops' / txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)
 
-                LOGGER.info(
-                    f'{s}Done. YOLO:({t3 - t2:.3f}s), DeepSort:({t5 - t4:.3f}s)')
+                LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s), DeepSort:({t5 - t4:.3f}s)')
 
             else:
                 deepsort_list[i].increment_ages()
@@ -244,18 +231,15 @@ def detect(opt):
                 if vid_path[i] != save_path:  # new video
                     vid_path[i] = save_path
                     if isinstance(vid_writer[i], cv2.VideoWriter):
-                        # release previous video writer
-                        vid_writer[i].release()
+                        vid_writer[i].release()  # release previous video writer
                     if vid_cap:  # video
                         fps = vid_cap.get(cv2.CAP_PROP_FPS)
                         w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                         h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                     else:  # stream
                         fps, w, h = 30, im0.shape[1], im0.shape[0]
-                    # force *.mp4 suffix on results videos
-                    save_path = str(Path(save_path).with_suffix('.mp4'))
-                    vid_writer[i] = cv2.VideoWriter(
-                        save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
+                    vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                 vid_writer[i].write(im0)
 
     # Print results
@@ -266,64 +250,38 @@ def detect(opt):
         s = f"\n{len(list(save_dir.glob('tracks/*.txt')))} tracks saved to {save_dir / 'tracks'}" if save_txt else ''
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
     if update:
-        # update model (to fix SourceChangeWarning)
-        strip_optimizer(yolo_model)
+        strip_optimizer(yolo_model)  # update model (to fix SourceChangeWarning)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo_model', nargs='+', type=str,
-                        default='yolov5m.pt', help='model.pt path(s)')
+    parser.add_argument('--yolo_model', nargs='+', type=str, default='yolov5m.pt', help='model.pt path(s)')
     parser.add_argument('--deep_sort_model', type=str, default='osnet_x0_25')
-    parser.add_argument('--source', type=str, default='0',
-                        help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='inference/output',
-                        help='output folder')  # output folder
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+',
-                        type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float,
-                        default=0.5, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float,
-                        default=0.5, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v',
-                        help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--device', default='',
-                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--show-vid', action='store_true',
-                        help='display tracking video results')
-    parser.add_argument('--save-vid', action='store_true',
-                        help='save video tracking results')
-    parser.add_argument('--save-txt', action='store_true',
-                        help='save MOT compliant results to *.txt')
+    parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.5, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--show-vid', action='store_true', help='display tracking video results')
+    parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
+    parser.add_argument('--save-txt', action='store_true', help='save MOT compliant results to *.txt')
     # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int,
-                        help='filter by class: --class 0, or --class 16 17')
-    parser.add_argument('--agnostic-nms', action='store_true',
-                        help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
-                        help='augmented inference')
-    parser.add_argument('--update', action='store_true',
-                        help='update all models')
-    parser.add_argument('--evaluate', action='store_true',
-                        help='augmented inference')
-    parser.add_argument("--config_deepsort", type=str,
-                        default="deep_sort/configs/deep_sort.yaml")
-    parser.add_argument("--half", action="store_true",
-                        help="use FP16 half-precision inference")
-    parser.add_argument('--visualize', action='store_true',
-                        help='visualize features')
-    parser.add_argument('--max-det', type=int, default=1000,
-                        help='maximum detection per image')
-    parser.add_argument('--save-crop', action='store_true',
-                        help='save cropped prediction boxes')
-    parser.add_argument('--dnn', action='store_true',
-                        help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--project', default=ROOT /
-                        'runs/track', help='save results to project/name')
-    parser.add_argument('--name', default='exp',
-                        help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true',
-                        help='existing project/name ok, do not increment')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 16 17')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--evaluate', action='store_true', help='augmented inference')
+    parser.add_argument("--config_deepsort", type=str, default="deep_sort/configs/deep_sort.yaml")
+    parser.add_argument("--half", action="store_true", help="use FP16 half-precision inference")
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detection per image')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--project', default=ROOT / 'runs/track', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
 
